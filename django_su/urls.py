01622777755f6cc6fa54dc05ab8829d1bb1b5103
gguerran <gustavoguerra.gr@gmail.com>
2022-01-24 00:29:21 -0300

    update url to repath

--- django_su/urls.py
+++ django_su/urls.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 
-try:
-    from django.conf.urls import url
-except ImportError:  # django < 1.4
-    from django.conf.urls.defaults import url
-
+from django.urls import re_path
 from .views import su_logout, su_login, login_as_user
 
 urlpatterns = [
-    url(r"^$", su_logout, name="su_logout"),
-    url(r"^login/$", su_login, name="su_login"),
-    url(r"^(?P<user_id>-?[\d]+)/$", login_as_user, name="login_as_user"),
+    re_path(r"^$", su_logout, name="su_logout"),
+    re_path(r"^login/$", su_login, name="su_login"),
+    re_path(r"^(?P<user_id>-?[\d]+)/$", login_as_user, name="login_as_user"),
 ]
